79a35b098fa28c83bba985fa4a7c9c6dd8ec5052
Luis Crespo <lcrespom@gmail.com>
2023-03-12 15:55:42 +0100

    Timer works

--- src/actions/sample.ts
+++ src/actions/sample.ts
@@ -1,13 +1,35 @@
-import { Action, BaseAction, KeyEvent } from '@stream-deck-for-node/sdk';
-import { sd } from '../index';
+import { Action, AppearDisappearEvent, BaseAction, KeyEvent } from '@stream-deck-for-node/sdk'
+import { sd } from '../index'
 
-let counter = 0;
+let counter = 0
 
 @Action('sample-action')
 export class SampleAction extends BaseAction {
+	ctx: string
+	visible = false
+	intervalId: NodeJS.Timer
 
-    onKeyDown(e: KeyEvent) {
-        sd.setTitle(e.context, 'Tap ' + ++counter);
-    }
+	constructor() {
+		super()
+		this.intervalId = setInterval(_ => this.updateButton(), 1000)
+	}
 
+	onAppear(e: AppearDisappearEvent) {
+		this.ctx = e.context
+		this.visible = true
+	}
+
+	onDisappear() {
+		this.visible = false
+		if (this.intervalId) clearInterval(this.intervalId)
+	}
+
+	onKeyDown(e: KeyEvent) {
+		sd.setTitle(e.context, 'Tap ' + ++counter)
+	}
+
+	updateButton() {
+		if (!this.visible) return
+		sd.setTitle(this.ctx, 'X ' + ++counter)
+	}
 }
