6be768d13f89f4ee2907e89f13fd29528460e04a
Luis Crespo <lcrespom@gmail.com>
2023-03-12 14:05:42 +0100

    Updated log function

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import * as fs from 'fs'
 import * as process from 'process'
 
 function log(msg: string) {
-	fs.appendFileSync('cpu.log', msg + '\n')
+	let now = new Date().toISOString().substring(11, 19)
+	fs.appendFileSync('cpu.log', now + ' ' + msg + '\n')
 }
 
-log(`Welcome to cpu-plugin - ${new Date()}`)
+log(`Welcome to cpu-plugin`)
 log(`Arguments: ${JSON.stringify(process.argv)}`)
 
 import './actions/sample'
